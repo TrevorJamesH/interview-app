0356932df6bc95e2462388440f38049b7371d0e3
Serafin <leaf466@gmail.com>
2017-04-14 14:20:12 -0700

    Tests all the props being in. Todo left behind for testing state

--- test/browser/molecules/form_test.js
+++ test/browser/molecules/form_test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { expect } from 'chai'
 import { shallow, mount, simulate } from 'enzyme'
 import { jsdom } from 'jsdom'
-import sinon from 'sinon'
 
 import Form from '../../../src/browser/components/molecules/form/index.js'
 import FormSelect from '../../../src/browser/components/atoms/form-select/index.js'
@@ -41,8 +40,7 @@ describe.only('<Form />', () => {
       expect(wrapper.find('form').exists()).to.equal.true
     })
 
-    it('should render SelectTag in the right order with correct props', () => {
-      const onChange = sinon.spy()
+    it('should render FormSelect in the right order with correct props', () => {
       const wrapper = mount(<Form inputModules={inputModules} />)
       expect(wrapper.find(Form).childAt(1).prop('prompt')).to.equal('What Is The Answer?')
       expect(wrapper.find(Form).childAt(1).prop('options')).to.contain('High')
@@ -50,22 +48,37 @@ describe.only('<Form />', () => {
       expect(wrapper.find(Form).childAt(1).prop('options')).to.not.contain('Mountain')
       expect(wrapper.find(Form).childAt(1).prop('isOptionRequired')).to.equal(false)
       expect(wrapper.find(Form).childAt(1).prop('isOptionRequired')).to.be.false
-      wrapper.find(FormSelect).simulate('change', {target: {value: 'Portside'}})
-      console.log('Before asynch')
-      setTimeout( () => {
-        console.log('I finally got it:', wrapper.state())
-      }, 25)
-      console.log('After asynch')
+      wrapper.find(FormSelect).simulate('change', {target: {value: 'Portside'}}) // TODO: Get this working, then use wrapper.state to view it
+      // console.log(wrapper.state())
       
       expect(wrapper.find(FormSelect).prop('options')).to.contain('Layside')
-      // expect(wrapper.find(Form).state())
     })
 
-    it('should render a button', () => {
-      const wrapper = shallow(<GameOptions difficulty={difficulty} topics ={topics}
-                            gameModes={gameModes} parse={{}} />)
-      expect(wrapper.find('button.uk-button').exists()).to.be.true
-
+    it('should render FormInput in the right order with correct props', () => {
+      const wrapper = mount(<Form inputModules={inputModules} />)
+      expect(wrapper.find(Form).childAt(0).prop('prompt')).to.equal('What Is Your Question?')
+      expect(wrapper.find(Form).childAt(0).prop('placeholder')).to.equal('Where is Voldemort')
     })
+
+    it('should render FormCheckbox in the right order with correct props', () => {
+      const wrapper = mount(<Form inputModules={inputModules} />)
+      expect(wrapper.find(Form).childAt(2).prop('prompt')).to.equal('Hot-Topic')
+      expect(wrapper.find(Form).childAt(2).prop('options')).to.contain('Hogwartzzz')
+      expect(wrapper.find(Form).childAt(2).prop('options')).to.contain('Castle Wolfenstein')
+      expect(wrapper.find(Form).childAt(2).prop('options')).to.not.contain('BlanketSleepers')
+    })
+
+    it('should render FormRadio in the right order with correct props', () => {
+      const wrapper = mount(<Form inputModules={inputModules} />)
+      expect(wrapper.find(Form).childAt(3).prop('prompt')).to.equal('Difficulty Levels')
+      expect(wrapper.find(Form).childAt(3).prop('options')).to.contain('Nemo')
+      expect(wrapper.find(Form).childAt(3).prop('options')).to.contain('Master')
+      expect(wrapper.find(Form).childAt(3).prop('options')).to.contain('Slave')
+      expect(wrapper.find(Form).childAt(3).prop('options')).to.contain('Jedi')
+      expect(wrapper.find(Form).childAt(3).prop('options')).to.not.contain('BeefMaster')
+    })
+
+    it('should render ')
+
   })
 })
